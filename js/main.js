85611e60b5dc95c41db7dfb1f62c00f418c9436b
Maxim <mmmaks_18@mail.ru>
2021-05-18 23:54:54 +0300

    Rebuilt project

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function toGitHub(){
+            window.open('https://github.com/Maxim-prog292');
+}
+function toInsta(){
+            window.open('https://www.instagram.com/tgysty/');
+}
+function toVK(){
+            window.open('https://vk.com/only_me_your_dad');
+}
+function writeMe() {
+    window.open('mailto:mmmaks_19@mail.ru?subject=Добрый%20день!')
+}
+function cellMe() {
+    window.open('tel:+7-900-588-50-27')
+}
