e9e2e8979f759670034d9231664fd4af4dcd8a3d
Maxim-prog292 <mcmagas00@gmail.com>
2021-06-30 12:18:56 +0300

    refactor: новое отображение карточек

--- js/main.js
+++ js/main.js
@@ -1,22 +1,34 @@
-function toGitHub(){
+let git = document.querySelector('.github'),
+    insta = document.querySelector('.insta'),
+    vk = document.querySelector('.vk'),
+    mail = document.querySelector('.mail'),
+    phone = document.querySelector('.phone');
+
+
+
+git.addEventListener('click', function toGitHub(){
             window.open('https://github.com/Maxim-prog292');
-}
-function toInsta(){
+});
+
+insta.addEventListener('click', function toInsta(){
             window.open('https://www.instagram.com/tgysty/');
-}
-function toVK(){
+});
+
+vk.addEventListener('click', function toVK(){
             window.open('https://vk.com/only_me_your_dad');
-}
-function writeMe() {
-    window.open('mailto:mmmaks_19@mail.ru?subject=Добрый%20день!')
-}
-function cellMe() {
-    window.open('tel:+7-900-588-50-27')
-}
+});
+
+mail.addEventListener('click', function writeMe() {
+    window.open('mailto:mmmaks_19@mail.ru?subject=Добрый%20день!');
+});
+
+phone.addEventListener('click', function cellMe() {
+    window.open('tel:+7-900-588-50-27');
+});
 
 window.onload = function() {
     let preloader = document.getElementById('preloader'),
         hidden = document.getElementById('hidden');       
     preloader.style.display = 'none';
     hidden.style.overflowY = 'scroll';
-}
+};
